9686d031e7a187fa92d2cb07b3aaf72e26034c51
General ABS <5gleo@allbesmart.pt>
2022-12-27 13:03:20 +0000

    Clean DLSCH after receiving the reconfigurationWithSync

--- openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
+++ openair1/PHY/NR_UE_TRANSPORT/nr_transport_proto_ue.h
@@ -253,6 +253,7 @@ uint8_t nr_ue_pusch_common_procedures(PHY_VARS_NR_UE *UE,
                                       NR_DL_FRAME_PARMS *frame_parms,
                                       uint8_t Nl);
 
+int8_t clean_UE_dlsch(PHY_VARS_NR_UE *UE, uint8_t gNB_id);
 int8_t clean_UE_ulsch(PHY_VARS_NR_UE *UE, uint8_t gNB_id);
 
 void nr_dlsch_unscrambling(int16_t* llr,
