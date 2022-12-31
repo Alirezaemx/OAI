b32d26b71fa3275233cca5e313f266ad22b8fcfd
General ABS <5gleo@allbesmart.pt>
2022-12-31 03:33:40 +0000

    Implementation of L3 measurement filtering

--- openair2/LAYER2/NR_MAC_UE/mac_proto.h
+++ openair2/LAYER2/NR_MAC_UE/mac_proto.h
@@ -112,6 +112,8 @@ void nr_rrc_mac_config_req_sib1(module_id_t module_id,
                                 int cc_idP,
                                 NR_ServingCellConfigCommonSIB_t *scc);
 
+void nr_rrc_mac_config_req_meas(module_id_t module_id, const NR_MeasConfig_t *measConfig);
+
 /**\brief initialization NR UE MAC instance(s), total number of MAC instance based on NB_NR_UE_MAC_INST*/
 NR_UE_MAC_INST_t * nr_l2_init_ue(NR_UE_RRC_INST_t* rrc_inst);
 
