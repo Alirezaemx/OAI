a35ede31189a3d053d99f2ab61513e055e3b9bf4
Robert Schmidt <robert.schmidt@openairinterface.org>
2023-06-07 18:24:50 +0200

    Fix dlopen() linker errors in dfts and ldpc

--- common/utils/minimal_stub.c
+++ common/utils/minimal_stub.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #ifndef T_TRACER
 int T_stdout;
 #endif
+int oai_exit = 0;
 
 void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
 {
